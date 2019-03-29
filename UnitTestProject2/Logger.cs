d85f78d48ebb80c2c77739f0a4bb4bcd6f850acc
Pavel Shyker <pavel_shyker@epam.com>
2019-03-29 15:00:12 +0300

    First commit

---
+++ UnitTestProject2/Logger.cs
@@ -0,0 +1,37 @@
+﻿using log4net;
+using log4net.Config;
+using OpenQA.Selenium;
+using System;
+using TestWebProject.Webdriver;
+
+namespace TestWebProject
+{
+
+    public static class Logger
+    {
+        public static ILog log = LogManager.GetLogger("LOGGER");
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        static Logger()
+        {
+            InitLogger();
+        }
+
+        public static void TakeScreenshot()
+        {
+            String fileName = "C:\\Users\\Pavel_Shyker\\CDPTasks\\CDPTaskModule13(1)\\UnitTestProject2\\Logs\\Image" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".jpg";
+            Log.Error("Image" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".jpg -> Screen is created");
+            Screenshot ss = ((ITakesScreenshot)Browser.GetDriver()).GetScreenshot();
+            ss.SaveAsFile(fileName, ScreenshotImageFormat.Png);
+        }
+    }
+}
---
+++ UnitTestProject2/Tests/MailRuMSTests.cs
@@ -0,0 +1,109 @@
+﻿namespace TestWebProject
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TestWebProject.Pages;
+    using TestWebProject.Webdriver;
+    using TestWebProject.BO;
+    using log4net;
+    using System;
+
+    [TestClass]
+    public class MailRuMSTests : BaseTest
+    {
+        private const string LogIn = "testuser.19";
+        private const string Password = "testCDP123";
+        private const string EmailAddress = "litmarsd@mail.ru";
+        private const string EmailSubject = "Subject";
+        private const string EmailText = "EmailTestText";
+
+
+        [TestMethod, TestCategory("Login")]
+        public void LoginTest()
+        {
+            try
+            {
+                var startPage = new StartPage();
+                startPage.Login(new User(LogIn, Password));
+                Assert.IsTrue(startPage.LoginSuccessMarker());
+            }
+            catch (Exception ex)
+            {
+                Logger.TakeScreenshot();
+                Logger.log.Error("Exception: " + ex);
+            }
+        }
+
+        [TestMethod, TestCategory("EmailCreating")]
+        public void CreateDraftEmailTest()
+        {
+            try
+            {
+                new StartPage().Login(new User(LogIn, Password));
+                new InboxPage().GoToNewEmailPage();
+
+                var emailPage = new EmailPage();
+                emailPage.CreateANewEmail(new Email(EmailAddress, EmailSubject, EmailText));
+                emailPage.SaveAsADraft();
+                emailPage.GoToDraftPage();
+
+                Assert.AreEqual(EmailAddress, new DraftPage().GetEmailAddress());
+            }
+            catch(Exception ex)
+            {
+                Logger.TakeScreenshot();
+                Logger.log.Error("Exception: " + ex);
+            }
+        }
+
+        [TestMethod, TestCategory("EmailSending")]
+        public void SendFolderAfterSendingTest()
+        {
+            try
+            {
+                new StartPage().Login(new User(LogIn, Password));
+                new InboxPage().GoToSentPage();
+
+                var sentPage = new SentPage();
+                sentPage.DeleteAllSent();
+                sentPage.GoToNewEmailPage();
+
+                var emailPage = new EmailPage();
+                emailPage.CreateANewEmail(new Email(EmailAddress, EmailSubject, EmailText));
+                emailPage.SaveAsADraft();
+                emailPage.GoToDraftPage();
+
+                new DraftPage().OpenEmail();
+
+                emailPage.SendEmail();
+                emailPage.GoToSentPage();
+
+                Assert.IsTrue(sentPage.SentEmailExist());
+            }
+
+            catch (Exception ex)
+            {
+                Logger.TakeScreenshot();
+                Logger.log.Error("Exception: " + ex);
+            }
+        }
+
+        [TestMethod, TestCategory("Logout")]
+        public void LogoutTest()
+        {
+            try
+            {
+                var startPage = new StartPage();
+                startPage.Login(new User(LogIn, Password));
+
+                new InboxPage().LogOut();
+
+                Assert.IsTrue(startPage.LogoutSuccessMarker());
+            }
+
+            catch (Exception ex)
+            {
+                Logger.log.Error("Exception: " + ex);
+            }
+        }
+    }
+}
